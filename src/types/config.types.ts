244c304a9cb2d3206075e4733cff2cf04a42e41b
ReeMusicTech <ricardo.aya.app@gmail.com>
2025-09-12 01:44:43 -0600

    Sep12-05

--- src/types/config.types.ts
+++ src/types/config.types.ts
@@ -163,3 +163,11 @@ export type OnceUIConfig = {
   effects: EffectsConfig;
   dataStyle: DataStyleConfig;
 };
+
+export interface Playlist {
+  // Define las propiedades que necesite tu playlist
+  id: string;
+  name: string;
+  tracks?: string[];
+  // ... otras propiedades que necesites
+}
